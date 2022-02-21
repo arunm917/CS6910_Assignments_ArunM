44320a98ffe3eeafbbcef9e933e6b36dc69677a0
arunm917 <arun.kumaran917@gmail.com>
2022-02-21 16:57:53 +0530

    Created hyperparameter file

--- Assignment_1/ForwardProp.py
+++ Assignment_1/ForwardProp.py
@@ -6,11 +6,6 @@ Created on Sat Feb 19 11:31:18 2022
 """
 
 import numpy as np
-from keras.datasets import fashion_mnist
-#print(dir(fashion_mnist))
-train, test = fashion_mnist.load_data()
-img, lbl = train
-import matplotlib.pyplot as plt
+import DataExtraction
 
-plt.imshow(img[6])
-print(lbl[6])
+
---
+++ Assignment_1/HyperParameters.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 16:55:33 2022
+
+@author: arunk
+"""
+
+class Hyperparameters():
+    L = 2       # Number of hidden layers
+    n = 512     # Number of neurons in hidden layer
+    
